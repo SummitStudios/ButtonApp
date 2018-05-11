79ddddd2848191e02ba3e1f2a01f56dbd1c0b554
Danny <danielvcameron@gmail.com>
2018-05-10 20:41:05 -0500

    Added static to numbers

--- ButtonApp/ViewController.swift
+++ ButtonApp/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         score = defaults.integer(forKey: "counterKey")
-        numberView.text = "\(score)"
+        numberView.text = String(score)
         
         // Do any additional setup after loading the view, typically from a nib.
     
